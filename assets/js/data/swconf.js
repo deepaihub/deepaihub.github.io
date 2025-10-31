2a5c52df8e64351713bd5863f6a0b7848575c9df
deepaihub <deepaihub@users.noreply.github.com>
2025-10-31 16:20:20 +0000

    deploy: 09d02558207d91c4e892c2af26bc4392903ce21d

--- assets/js/data/swconf.js
+++ assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1761926702',resources: [
+    cacheName: 'chirpy-1761927618',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
